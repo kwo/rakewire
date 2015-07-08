55ccfad884e0fd448efca4793eb7478465c51f3f
Karl Ostendorf <karl@ostendorf.com>
2015-07-08 17:59:45 +0200

    update test

--- model/configuration_test.go
+++ model/configuration_test.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
@@ -21,5 +22,6 @@ func TestConfiguration(t *testing.T) {
 	assert.Equal(t, "0.0.0.0", c.Httpd.Address)
 	assert.Equal(t, 4444, c.Httpd.Port)
 	assert.Equal(t, "/Users/karl/static", c.Httpd.WebAppDir)
+	assert.Equal(t, "0.0.0.0:4444", fmt.Sprintf("%s:%d", c.Httpd.Address, c.Httpd.Port))
 
 }
