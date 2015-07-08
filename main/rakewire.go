703e18337112d3e295328ccfcdd17628db370c4d
Karl Ostendorf <karl@ostendorf.com>
2015-07-08 17:59:59 +0200

    use main to start httpd

--- main/rakewire.go
+++ main/rakewire.go
@@ -4,21 +4,51 @@ import (
 	"fmt"
 	"os"
 	"path"
-	"rakewire.com/fetch"
-	"strings"
+	m "rakewire.com/model"
+	"rakewire.com/server"
+)
+
+const (
+	configFileName = "config.yaml"
 )
 
 func main() {
 
-	var param string
-	if len(os.Args[1:]) == 1 {
-		param = strings.TrimSpace(os.Args[1])
-	}
-	if len(param) == 0 {
-		fmt.Printf("Usage: %s <feed-file>\n", path.Base(os.Args[0]))
+	cfg := getConfig()
+	if cfg == nil {
+		fmt.Printf("Abort! No config file found at %s\n", getConfigFileLocation())
 		os.Exit(1)
+		return
 	}
 
-	fetch.Fetch(param)
+	server.Serve(cfg.Httpd)
 
+}
+
+func getHomeDirectory() string {
+	homeLocations := []string{"HOME", "HOMEPATH", "USERPROFILE"}
+	for _, v := range homeLocations {
+		x := os.Getenv(v)
+		if x != "" {
+			return x
+		}
+	}
+	return ""
+}
+
+func getConfigFileLocation() string {
+	var home = getHomeDirectory()
+	if home != "" {
+		return path.Join(getHomeDirectory(), ".config", "rakewire", configFileName)
+	}
+	return configFileName
+}
+
+func getConfig() *m.Configuration {
+	cfg := m.Configuration{}
+	err := cfg.LoadFromFile(getConfigFileLocation())
+	if err != nil {
+		return nil
+	}
+	return &cfg
 }
---
+++ main/rakewire_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestHomeDirectory(t *testing.T) {
+	assert.Equal(t, "/Users/karl", getHomeDirectory())
+}
+
+func TestConfigFileLocation(t *testing.T) {
+	assert.Equal(t, "/Users/karl/.config/rakewire/config.yaml", getConfigFileLocation())
+}
+
+func TestConfig(t *testing.T) {
+	assert.Nil(t, getConfig())
+}
