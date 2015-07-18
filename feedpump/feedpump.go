e1458e9d858cd0c082d2a99836b6e95b818c8948
Karl Ostendorf <karl@ostendorf.com>
2015-07-18 11:38:49 +0200

    add skeleton feedpump

--- feedpump/feedpump.go
+++ feedpump/feedpump.go
@@ -3,7 +3,105 @@ package feedpump
 import (
 	"rakewire.com/db"
 	"rakewire.com/fetch"
+	"rakewire.com/logging"
+	"sync/atomic"
 )
+
+const (
+	numGoRoutines = 2
+)
+
+var (
+	logger = logging.New("feedpump")
+)
+
+// NewService create a new service
+func NewService(cfg *Configuration) *Service {
+
+	ch := &fetch.Channels{
+		Requests:  make(chan *fetch.Request),
+		Responses: make(chan *fetch.Response),
+	}
+
+	return &Service{
+		Channels:       ch,
+		countdownLatch: make(chan bool),
+	}
+
+}
+
+// Configuration for pump service
+type Configuration struct {
+}
+
+// Service for pumping feeds between fetcher and database
+type Service struct {
+	Channels       *fetch.Channels
+	killsignal     int32
+	countdownLatch chan bool
+}
+
+// Start Service
+func (z *Service) Start(chErrors chan error) {
+
+	logger.Println("service starting...")
+
+	z.setRunning(true)
+	go z.saveResponses()
+	go z.fetchRequests()
+
+	logger.Println("service started.")
+
+}
+
+// Stop service
+func (z *Service) Stop() {
+	logger.Println("service stopping...")
+	z.setRunning(false)
+	for i := 0; i < numGoRoutines; i++ {
+		<-z.countdownLatch
+	}
+	logger.Println("service stopped.")
+}
+
+func (z *Service) fetchRequests() {
+
+	logger.Println("fetchRequests starting...")
+
+	for z.IsRunning() {
+
+	}
+
+	logger.Println("fetchRequests exited.")
+	z.countdownLatch <- true
+
+}
+
+func (z *Service) saveResponses() {
+
+	logger.Println("saveResponses starting...")
+
+	for z.IsRunning() {
+
+	}
+
+	logger.Println("saveResponses exited.")
+	z.countdownLatch <- true
+
+}
+
+// IsRunning status of the service
+func (z *Service) IsRunning() bool {
+	return atomic.LoadInt32(&z.killsignal) != 0
+}
+
+func (z *Service) setRunning(running bool) {
+	if running {
+		atomic.StoreInt32(&z.killsignal, 1)
+	} else {
+		atomic.StoreInt32(&z.killsignal, 0)
+	}
+}
 
 func databaseFeedsToFetchRequests(dbfeeds *db.Feeds) []*fetch.Request {
 	var feeds []*fetch.Request
---
+++ feedpump/pump_test.go
@@ -0,0 +1,23 @@
+package feedpump
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestFetch(t *testing.T) {
+
+	//t.SkipNow()
+
+	cfg := &Configuration{}
+
+	pump := NewService(cfg)
+	chErrors := make(chan error)
+
+	pump.Start(chErrors)
+	require.Equal(t, true, pump.IsRunning())
+	pump.Stop()
+	assert.Equal(t, false, pump.IsRunning())
+
+}
