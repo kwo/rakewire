c5f3fe8f00f60f7008ebc661f5a0de936ed5d7a6
Karl Ostendorf <karl@ostendorf.com>
2015-07-21 11:24:58 +0200

    add ticker tests

--- pollfeed/pollfeed_test.go
+++ pollfeed/pollfeed_test.go
@@ -7,13 +7,53 @@ import (
 	"rakewire.com/db"
 	"rakewire.com/db/bolt"
 	"testing"
+	"time"
 )
 
 const (
 	databaseFile = "../test/pollfeed.db"
 )
 
+func TestTickerKillSignal(t *testing.T) {
+
+	beenThere := false
+	killsignal := make(chan bool)
+	ticker := time.NewTicker(5 * time.Minute)
+	go func() {
+		select {
+		case <-ticker.C:
+			assert.Fail(t, "ticker should never fire")
+		case <-killsignal:
+			ticker.Stop()
+			break
+		}
+		beenThere = true
+	}()
+	killsignal <- true
+	assert.True(t, beenThere)
+
+}
+
+func TestTickerPositive(t *testing.T) {
+
+	beenThere := false
+	ticker := time.NewTicker(1 * time.Millisecond)
+	go func() {
+		select {
+		case <-ticker.C:
+			beenThere = !beenThere
+			ticker.Stop()
+			break
+		}
+	}()
+	time.Sleep(2 * time.Millisecond)
+	assert.True(t, beenThere)
+
+}
+
 func TestFetch(t *testing.T) {
+
+	t.SkipNow()
 
 	// open database
 	database := &bolt.Database{}
