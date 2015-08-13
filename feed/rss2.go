e76148bc007a2eca0800d5f5e138a8c8822b7aae
Karl Ostendorf <karl@ostendorf.com>
2015-08-13 10:50:14 +0200

    move to ocd package

--- feed/rss2.go
+++ feed/rss2.go
@@ -1,54 +1,32 @@
 package feed
 
 import (
-	"encoding/xml"
-	"time"
+	"github.com/kwo/ocd/feeds/rss"
 )
 
-type rssFeed struct {
-	XMLName xml.Name
-	Channel rssChannel `xml:"channel"`
-	Version string     `xml:"version,attr"`
-}
-
-type rssChannel struct {
-	Description string    `xml:"description"`
-	Generator   string    `xml:"generator"`
-	Items       []rssItem `xml:"item"`
-	Link        string    `xml:"rss link"`
-	Title       string    `xml:"title"`
-}
-
-type rssItem struct {
-	Content string    `xml:"content"`
-	Created time.Time `xml:"pubDate"`
-	ID      string    `xml:"guid"`
-	Summary string    `xml:"description"`
-	Title   string    `xml:"title"`
-	Updated time.Time `xml:"updated"`
-}
-
-func (r rssFeed) toFeed() (*Feed, error) {
+func rssToFeed(r *rss.Rss) (*Feed, error) {
 
 	f := &Feed{}
 
 	f.ID = r.Channel.Link
 	f.Title = r.Channel.Title
 	f.Subtitle = r.Channel.Description
-	f.Flavor = "rss" + r.Version
+	f.Flavor = "rss2"
 	f.Generator = r.Channel.Generator
 
-	for _, rssItem := range r.Channel.Items {
+	for i := range r.Channel.Items {
 
 		entry := &Entry{}
 		f.Entries = append(f.Entries, entry)
 
-		entry.Title = rssItem.Title
-		*entry.Created = rssItem.Created
-		*entry.Updated = rssItem.Updated
-		if entry.Updated.IsZero() {
-			entry.Updated = entry.Created
-		}
+		entry.Title = r.Channel.Items[i].Title
+		dt := r.Channel.Items[i].PubDate.GetTime()
+		entry.Updated = &dt
+		//entry.Created = rssItem.Created
+		//entry.Updated = rssItem.Updated
+		// if entry.Updated.IsZero() {
+		// 	entry.Updated = entry.Created
+		// }
 		//entry.Author = &Author{atomEntry.Author.Name, atomEntry.Author.EMail, atomEntry.Author.URI}
 
 		// for j := 0; j < len(atomEntry.Links); j++ {
@@ -61,16 +39,14 @@ func (r rssFeed) toFeed() (*Feed, error) {
 		// 	entry.Categories = append(entry.Categories, atomEntry.Categories[j].String())
 		// }
 
-		entry.Summary = rssItem.Summary
-		entry.Content = rssItem.Content
+		// entry.Summary = rssItem.Summary
+		// entry.Content = rssItem.Content
 
 	} // loop
 
 	if len(f.Entries) > 0 {
 		f.Updated = f.Entries[0].Updated
 	}
-
-	logger.Printf("RSS SPACE:!%s!", r.XMLName.Space)
 
 	return f, nil
 
