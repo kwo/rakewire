de5a984c64d9bbf2cc1c13138b00c1ca10da4aa5
Karl Ostendorf <karl@ostendorf.com>
2015-08-18 11:40:12 +0200

    use gb file structure

---
+++ vendor/src/github.com/kwo/ocd/feeds/modules/content/content.go
@@ -0,0 +1,6 @@
+package content
+
+// Content module from http://web.resource.org/rss/1.0/modules/content/
+type Content struct {
+	Encoded string `xml:"http://purl.org/rss/1.0/modules/content/ encoded"`
+}
