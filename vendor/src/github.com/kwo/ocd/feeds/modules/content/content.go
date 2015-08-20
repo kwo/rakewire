e0461abe9b5e1196b549e69ef0fc1443162bdf92
Karl Ostendorf <karl@ostendorf.com>
2015-08-20 17:27:35 +0200

    remove feed package

--- vendor/src/github.com/kwo/ocd/feeds/modules/content/content.go
+++
@@ -1,6 +0,0 @@
-package content
-
-// Content module from http://web.resource.org/rss/1.0/modules/content/
-type Content struct {
-	Encoded string `xml:"http://purl.org/rss/1.0/modules/content/ encoded"`
-}
