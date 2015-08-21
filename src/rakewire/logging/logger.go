3dc9543b7068e33ba84f69400fdbc8754d4484e6
Karl Ostendorf <karl@ostendorf.com>
2015-08-21 17:43:53 +0200

    add null logger

--- src/rakewire/logging/logger.go
+++ src/rakewire/logging/logger.go
@@ -23,7 +23,13 @@ func New(category string) *log.Logger {
 	return log.New(output, fmt.Sprintf("%-9.8s", category), log.Ltime)
 }
 
-// New create a new internal logger
+// Null create a new internal logger
+func Null(category string) *log.Logger {
+	tmp, _ := os.Open(os.DevNull)
+	return log.New(tmp, fmt.Sprintf("%-9.8s", category), log.Ltime)
+}
+
+// NewFromType create a new internal logger
 func NewFromType(a interface{}) *log.Logger {
 	return New(fmt.Sprintf("%-9.8s", path.Base(reflect.TypeOf(a).PkgPath())))
 }
