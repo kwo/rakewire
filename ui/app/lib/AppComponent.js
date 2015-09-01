2a31c861e1fc64fc11c82673282debc178518b0f
Karl Ostendorf <karl@ostendorf.com>
2015-09-01 17:21:35 +0200

    create AppBar with tabs

--- ui/app/lib/AppComponent.js
+++ ui/app/lib/AppComponent.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link, RouteHandler } from 'react-router';
-import { AppBar, IconButton, Styles } from 'material-ui';
+import { AppBar, IconButton, Styles, Tab, Tabs } from 'material-ui';
 const ThemeManager = new Styles.ThemeManager();
 
 class App extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {};
+	constructor(props, context) {
+		super(props, context);
+		this.state = {
+			tab: 'home'
+		};
+		this.onLogoClick = this.onLogoClick.bind(this);
+		this.onChangeTabs = this.onChangeTabs.bind(this);
+		this.navigateTo = this.navigateTo.bind(this);
 	}
 
 	getChildContext () {
@@ -16,11 +21,32 @@ class App extends React.Component {
 		};
 	}
 
-	onLogoClick(event) {
-		console.log(event)
+	navigateTo(name) {
+		const state = this.state;
+		state.tab = name;
+		this.setState(state);
+		this.context.router.transitionTo(state.tab);
+	}
+
+	onChangeTabs(name /*, event, tab*/) {
+		this.navigateTo(name);
+	}
+
+	onLogoClick(/*event*/) {
+		this.navigateTo('home');
 	}
 
 	render() {
+
+		const styles = {
+			appBar: {
+				flexWrap: 'wrap',
+			},
+			tabs: {
+				width: '25%',
+			}
+		};
+
 
 		const logoButton = (
 			<IconButton
@@ -34,11 +60,21 @@ class App extends React.Component {
 
 		return (
 			<div>
+
 				<AppBar
 					title="Rakewire"
 					iconElementLeft={logoButton}
-					/>
+					style={styles.appBar}>
+					<Tabs
+						value={this.state.tab}
+						onChange={this.onChangeTabs} style={styles.tabs}>
+						<Tab label="Home" value="home" />
+						<Tab label="About" value="about" />
+					</Tabs>
+				</AppBar>
+
 				<RouteHandler />
+
 			</div>
 		);
 
@@ -47,6 +83,10 @@ class App extends React.Component {
 }
 
 App.displayName = 'app';
+
+App.contextTypes = {
+	router: React.PropTypes.func
+};
 
 // only necessary by App (outermost parent component)
 App.childContextTypes = {
