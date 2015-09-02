11b5a97ce391bcae4ea0589d05e6e09d0e90c4bc
Karl Ostendorf <karl@ostendorf.com>
2015-09-02 08:37:13 +0200

    combine routes and main (again)

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
  - #DOING:0 html ui
  - #DOING:2 make routes into Routes, pass ref of runction to instance and pass to App (should Routes be a react component?)
  - #DOING:3 getChildContext only on grandfather
+ - #DOING:4 order of methods
  - #TODO:40 load roboto font locally
  - #DOING:20 add version stamp to app.{js,css}
  - #TODO:0 better logging
--- ui/app/lib/main.js
+++ ui/app/lib/main.js
@@ -1,6 +1,7 @@
 import 'site.css!'; // #TODO:0 css is missing entries for individual icons
 
 import React from 'react';
+import Router, {DefaultRoute, Redirect, Route} from 'react-router';
 import { Styles } from 'material-ui';
 import injectTapEventPlugin from 'npm:react-tap-event-plugin@0.1.7/src/injectTapEventPlugin';
 
@@ -8,8 +9,19 @@ const ThemeManager = new Styles.ThemeManager();
 ThemeManager.setTheme(ThemeManager.types.LIGHT);
 injectTapEventPlugin();
 
-import Router from 'react-router';
-import routes from './routes';
+import App from './App';
+import About from './About';
+import Home from './Home';
+
+const routes = (
+	<Route handler={App} name="app" path="/" >
+		<Route handler={Home} name="home" path="/" />
+		<Route handler={About} name="about" path="/about" />
+		<DefaultRoute handler={Home} />
+		<Redirect from="*" to="home" />
+	</Route>
+);
+
 Router.run(routes, Router.HistoryLocation, function(Handler) {
 	React.render(<Handler />, document.getElementById('app'));
 });
