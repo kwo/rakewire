fc47870e07957c3a8fe20764fe7db14ea0540ff6
Karl Ostendorf <karl@ostendorf.com>
2015-09-11 14:42:50 +0200

    move vendored deps back to gb-style directory
    vendor expirement not yet supported by many tools, z.B. go-pro atom editor

--- README.md
+++ README.md
@@ -9,23 +9,22 @@ This source code is divided into two projects: the backend, written in go, and t
 ### Prepare Environment
 
 The project root directory ($PROJECT_ROOT) acts as the GOPATH and contains the bin, pkg and src directories.
-The project dependencies are located at src/rakewire/vendor according to the GO15VENDOREXPERIMENT=1 convention.
+The project dependencies are located at vendor/src according to the gb convention.
 
 Therefore, set the following environment varibles:
 
-	export GOPATH=$PROJECT_ROOT
-	export GOBIN=$GOPATH/bin
-	export GO15VENDOREXPERIMENT=1
+	export GOPATH=$PROJECT_ROOT:$PROJECT_ROOT/vendor
+	export GOBIN=$PROJECT_ROOT/bin
 
 ### Build
 
 	cd $PROJECT_ROOT
-	go build src/rakewire/rakewire.go
+	go build ./src/rakewire/rakewire.go
 
 ### Run
 
 	cd $PROJECT_ROOT
-	go run src/rakewire/rakewire.go
+	go run ./src/rakewire/rakewire.go
 
 ### Top-Level Dependencies
 
--- build.sh
+++ build.sh
@@ -4,35 +4,37 @@ CMD=$1
 
 case $CMD in
 
-	"run")
-		go run ./src/rakewire/rakewire.go
-	  ;;
-
-	"test")
-		go test $(go list ./src/rakewire/... | grep -v /vendor/)
-	  ;;
+	"clean")
+		git clean -fdx
+		;;
 
 	"build")
 		go build ./src/rakewire/rakewire.go
+	  ;;
+
+	"depgraph")
+		godepgraph -s -horizontal rakewire | dot -Tsvg -o depgraph.svg && open depgraph.svg
 	  ;;
 
 	"install")
 		go install ./src/rakewire/rakewire.go
 	  ;;
 
-	"update")
-		cd $GOPATH/src/rakewire
-		gvt update -all
+	"run")
+		go run ./src/rakewire/rakewire.go
 	  ;;
 
-	"depgraph")
-		godepgraph -s rakewire | dot -Tsvg -o depgraph.svg
-		open depgraph.svg
+	"test")
+		go test ./src/...
+	  ;;
+
+	"update")
+		gb vendor update --all
 	  ;;
 
   *)
 	  echo "unknown command: $CMD"
-		echo "Usage `basename $0`: run | test | build | install | update | depgraph"
+		echo "Usage `basename $0`: clean | build | depgraph | install | run | test | update"
 		;;
 
 esac
