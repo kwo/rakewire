9b19136803b253debe9f39af2f84b20c9088a555
Karl Ostendorf <karl@ostendorf.com>
2015-09-15 20:01:55 +0200

    start on feed log

--- ui/app/lib/Feed.js
+++ ui/app/lib/Feed.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+//import moment from 'moment';
 
 class Feed extends React.Component {
 
@@ -26,16 +27,23 @@ class Feed extends React.Component {
 
 	componentDidMount() {
 		if (this.props.params.id) {
-			fetch(this.context.config.rootURL + '/feeds/' + this.props.params.id)
-				.then(rsp => rsp.json())
-				.then(feed => {
-					this.setState({
-						feed: feed
-					});
-				})
-				.catch(e => {
-					console.error(e); // XXX: display error in UI
+			const feedURL = this.context.config.rootURL + '/feeds/' + this.props.params.id;
+			const feedLogURL = feedURL + '/log';
+			Promise.all([
+				fetch(feedURL),
+				fetch(feedLogURL)
+			])
+			.then(results => {
+				const feed = results[0].json();
+				feed.log = results[1].json();
+				return feed;
+			})
+			.then(feed => {
+				this.setState({
+					feed: feed
 				});
+			})
+			.catch(e => console.error(e)); // XXX: display error in UI
 		} // id
 	}
 
@@ -47,10 +55,48 @@ class Feed extends React.Component {
 			);
 		}
 
+		const style = {
+			table: {
+				width: '100%'
+			},
+			td: {
+				height: 24,
+				paddingLeft: 12,
+				paddingRight: 12,
+				textAlign: 'left',
+				width: 40
+			},
+			th: {
+				paddingLeft: 12,
+				paddingRight: 12,
+				textAlign: 'left',
+				width: 40
+			}
+		};
+
+		const feed = this.state.feed;
+		const title = feed.title || feed.last200.feed.title || feed.url;
+
 		return (
-			<div>
-				{this.state.feed.id}: {this.state.feed.title}
-			</div>
+
+			<table style={style.table}>
+
+				<thead>
+
+					<tr key={0}>
+						<th style={style.th}>Title</th>
+						<td style={style.td}>{title}</td>
+					</tr>
+
+					<tr key={1}>
+						<th style={style.th}>URL</th>
+						<td style={style.td}>{feed.url}</td>
+					</tr>
+
+				</thead>
+
+			</table>
+
 		);
 
 	}
