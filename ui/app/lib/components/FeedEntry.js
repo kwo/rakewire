1318195985442172ba9ba604b57b607adb89015d
Karl Ostendorf <karl@ostendorf.com>
2015-09-16 20:56:28 +0200

    add LastUpdated to FeedEntry

--- ui/app/lib/components/FeedEntry.js
+++ ui/app/lib/components/FeedEntry.js
@@ -32,30 +32,45 @@ class FeedEntry extends React.Component {
 	render() {
 
 		const style = {
-			td: {
-				cursor: 'pointer',
-				height: 24,
-				paddingLeft: 12,
-				paddingRight: 12,
-				textAlign: 'left',
-				width: 40
-			},
-			tdFeed: {
-				cursor: 'pointer',
-				height: 24,
-				textAlign: 'left',
-				width: '50%'
-			},
 			th: {
-				paddingLeft: 12,
-				paddingRight: 12,
+				paddingLeft: 6,
+				paddingRight: 6,
 				textAlign: 'left',
-				width: 40
+				width: '40px'
+			},
+			thLU: {
+				paddingLeft: 6,
+				paddingRight: 6,
+				textAlign: 'left',
+				width: '50px'
 			},
 			thFeed: {
+				paddingLeft: 6,
+				paddingRight: 6,
 				textAlign: 'left',
-				width: '50%'
+				width: '40%'
 			},
+			td: {
+				height: 24,
+				paddingLeft: 6,
+				paddingRight: 6,
+				textAlign: 'left',
+				width: '40px'
+			},
+			tdLU: {
+				height: 24,
+				paddingLeft: 6,
+				paddingRight: 6,
+				textAlign: 'left',
+				width: '50px'
+			},
+			tdFeed: {
+				height: 24,
+				paddingLeft: 6,
+				paddingRight: 6,
+				textAlign: 'left',
+				width: '40%'
+			}
 		};
 
 		const feed = this.props.feed;
@@ -69,12 +84,17 @@ class FeedEntry extends React.Component {
 					<TableHeaderColumn style={style.th}>Code</TableHeaderColumn>
 					<TableHeaderColumn style={style.th}>Updated</TableHeaderColumn>
 					<TableHeaderColumn style={style.th}>Check</TableHeaderColumn>
+					<TableHeaderColumn style={style.thLU}>Last Updated</TableHeaderColumn>
 					<TableHeaderColumn style={style.thFeed}>Feed</TableHeaderColumn>
 				</TableRow>
 			);
 		}
 
 		const formatDate = function(dt) {
+			return moment(dt).format('YYYY-MM-DD');
+		};
+
+		const formatTime = function(dt) {
 			return moment(dt).format('dd HH:mm');
 		};
 
@@ -88,12 +108,13 @@ class FeedEntry extends React.Component {
 			<TableRow
 				hoverable={true}
 				onRowClick={this.onRowClick}>
-				<TableRowColumn style={style.td}>{formatDate(feed.nextFetch)}</TableRowColumn>
-				<TableRowColumn style={style.td}>{formatDate(feed.last.startTime)}</TableRowColumn>
+				<TableRowColumn style={style.td}>{formatTime(feed.nextFetch)}</TableRowColumn>
+				<TableRowColumn style={style.td}>{formatTime(feed.last.startTime)}</TableRowColumn>
 				<TableRowColumn style={style.td}>{status}</TableRowColumn>
 				<TableRowColumn style={style.td}>{feed.last.http.statusCode}</TableRowColumn>
 				<TableRowColumn style={style.td}>{isUpdated}</TableRowColumn>
 				<TableRowColumn style={style.td}>{updateCheck}</TableRowColumn>
+				<TableRowColumn style={style.tdLU}>{formatDate(feed.lastUpdated)}</TableRowColumn>
 				<TableRowColumn style={style.tdFeed}><Link to={feedLinkURL}>{title}</Link></TableRowColumn>
 			</TableRow>
 		);
