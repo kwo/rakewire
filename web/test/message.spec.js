a846f28ce3ddb37f6f061a8ec4cc5ce29dcf1933
Karl Ostendorf <karl@ostendorf.com>
2015-10-27 14:07:10 +0100

    replace mocha and chai with tape

--- web/test/message.spec.js
+++ web/test/message.spec.js
@@ -1,41 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
-import {assert} from 'chai';
+import test from 'tape';
+
 import Message from '../app/components/Message.jsx';
 
-describe('Message', () => {
+test('message rendered with default message', t => {
 
-	it('message rendered with default message', done => {
+	const msg = TestUtils.renderIntoDocument(<Message/>);
 
-		const msg = TestUtils.renderIntoDocument(<Message/>);
+	const h4Elements = TestUtils.scryRenderedDOMComponentsWithTag(msg, 'h4');
+	t.equal(1, h4Elements.length);
+	const h4Node = ReactDOM.findDOMNode(h4Elements[0]);
+	t.equal('Unconfigured message!', h4Node.innerHTML);
 
-		const h4Elements = TestUtils.scryRenderedDOMComponentsWithTag(msg, 'h4');
-		assert.strictEqual(1, h4Elements.length);
-		const h4Node = ReactDOM.findDOMNode(h4Elements[0]);
-		assert.strictEqual('Unconfigured message!', h4Node.innerHTML);
+	t.end();
 
-		done();
+});
 
-	});
+test('button should not be rendered', t => {
+	const msg = TestUtils.renderIntoDocument(<Message />);
+	t.equal(0, TestUtils.scryRenderedDOMComponentsWithTag(msg, 'button').length);
+	t.end();
+});
 
-	it('button should not be rendered', done => {
-		const msg = TestUtils.renderIntoDocument(<Message />);
-		assert.strictEqual(0, TestUtils.scryRenderedDOMComponentsWithTag(msg, 'button').length);
-		done();
-	});
+test('message rendered with message', t => {
 
-	it('message rendered with message', done => {
+	const text = 'message text';
+	const msg = TestUtils.renderIntoDocument(<Message message={text} />);
 
-		const text = 'message text';
-		const msg = TestUtils.renderIntoDocument(<Message message={text} />);
+	const h4 = TestUtils.findRenderedDOMComponentWithTag(msg, 'h4');
+	const h4Node = ReactDOM.findDOMNode(h4);
+	t.equal(text, h4Node.innerHTML);
 
-		const h4 = TestUtils.findRenderedDOMComponentWithTag(msg, 'h4');
-		const h4Node = ReactDOM.findDOMNode(h4);
-		assert.strictEqual(text, h4Node.innerHTML);
-
-		done();
-
-	});
+	t.end();
 
 });
