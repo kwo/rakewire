d676f15832aad558ea84ad354aca8a65eb47c8af
Karl Ostendorf <karl@ostendorf.com>
2015-10-27 13:37:40 +0100

    replace should with chai

--- web/test/message.spec.js
+++ web/test/message.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
-import should from 'should';
+import {assert} from 'chai';
 import Message from '../app/components/Message.jsx';
 
 describe('Message', () => {
@@ -11,9 +11,9 @@ describe('Message', () => {
 		const msg = TestUtils.renderIntoDocument(<Message/>);
 
 		const h4Elements = TestUtils.scryRenderedDOMComponentsWithTag(msg, 'h4');
-		should.equal(1, h4Elements.length);
+		assert.strictEqual(1, h4Elements.length);
 		const h4Node = ReactDOM.findDOMNode(h4Elements[0]);
-		should.equal('Unconfigured message!', h4Node.innerHTML);
+		assert.strictEqual('Unconfigured message!', h4Node.innerHTML);
 
 		done();
 
@@ -21,7 +21,7 @@ describe('Message', () => {
 
 	it('button should not be rendered', done => {
 		const msg = TestUtils.renderIntoDocument(<Message />);
-		should.equal(0, TestUtils.scryRenderedDOMComponentsWithTag(msg, 'button').length);
+		assert.strictEqual(0, TestUtils.scryRenderedDOMComponentsWithTag(msg, 'button').length);
 		done();
 	});
 
@@ -32,7 +32,7 @@ describe('Message', () => {
 
 		const h4 = TestUtils.findRenderedDOMComponentWithTag(msg, 'h4');
 		const h4Node = ReactDOM.findDOMNode(h4);
-		should.equal(text, h4Node.innerHTML);
+		assert.strictEqual(text, h4Node.innerHTML);
 
 		done();
 
