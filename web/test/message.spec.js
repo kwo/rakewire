91288f44d5b3e7cdfefa55c7ae59d97edee80802
Karl Ostendorf <karl@ostendorf.com>
2015-10-27 13:31:00 +0100

    add more message tests

--- web/test/message.spec.js
+++ web/test/message.spec.js
@@ -10,12 +10,19 @@ describe('Message', () => {
 
 		const msg = TestUtils.renderIntoDocument(<Message/>);
 
-		const h4 = TestUtils.findRenderedDOMComponentWithTag(msg, 'h4');
-		const h4Node = ReactDOM.findDOMNode(h4);
+		const h4Elements = TestUtils.scryRenderedDOMComponentsWithTag(msg, 'h4');
+		should.equal(1, h4Elements.length);
+		const h4Node = ReactDOM.findDOMNode(h4Elements[0]);
 		should.equal('Unconfigured message!', h4Node.innerHTML);
 
 		done();
 
+	});
+
+	it('button should not be rendered', done => {
+		const msg = TestUtils.renderIntoDocument(<Message />);
+		should.equal(0, TestUtils.scryRenderedDOMComponentsWithTag(msg, 'button').length);
+		done();
 	});
 
 	it('message rendered with message', done => {
