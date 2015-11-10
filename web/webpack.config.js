c0577f9462b125fc3423ac65d4a98db1854f0a10
Karl Ostendorf <karl@ostendorf.com>
2015-11-10 15:51:35 +0100

    upgrade to babel 6

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -16,7 +16,14 @@ const config = {
 	module: {
 		loaders: [
 			{ test: /\.css$/,  loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
-			{ test: /\.jsx?$/, loader: 'babel?optional[]=runtime&stage=0', exclude: /node_modules/ },
+			{ test: /\.jsx?$/,
+				loader: 'babel',
+				exclude: /node_modules/,
+				query: {
+					cacheDirectory: true,
+					presets: ['es2015', 'react', 'stage-0']
+				}
+			},
 			{ test: /\.jsx?$/, loader: 'eslint-loader', exclude: /node_modules/ }
 		]
 	},
