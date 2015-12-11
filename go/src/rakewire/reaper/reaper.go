19074b25ef31cf8030685bb276e42151a88d7a2b
Karl Ostendorf <karl@ostendorf.com>
2015-12-11 09:21:24 +0100

    downgrade log statement to DEBUG level

--- go/src/rakewire/reaper/reaper.go
+++ go/src/rakewire/reaper/reaper.go
@@ -170,7 +170,7 @@ func (z *Service) processResponse(feed *m.Feed) {
 		return
 	}
 
-	log.Printf("%-7s %-7s %2s  %3d  %s  %3d/%-3d  %s  %s", logInfo, logName, feed.Status, feed.Attempt.StatusCode, feed.LastUpdated.Local().Format("02.01.06 15:04"), feed.Attempt.NewEntries, feed.Attempt.EntryCount, feed.URL, feed.StatusMessage)
+	log.Printf("%-7s %-7s %2s  %3d  %s  %3d/%-3d  %s  %s", logDebug, logName, feed.Status, feed.Attempt.StatusCode, feed.LastUpdated.Local().Format("02.01.06 15:04"), feed.Attempt.NewEntries, feed.Attempt.EntryCount, feed.URL, feed.StatusMessage)
 
 }
 
