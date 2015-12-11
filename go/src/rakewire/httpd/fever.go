c0394ebf1a68135d29a2b0b3844645242e830a73
Karl Ostendorf <karl@ostendorf.com>
2015-12-11 16:39:29 +0100

    fever api marshall json and xml

--- go/src/rakewire/httpd/fever.go
+++ go/src/rakewire/httpd/fever.go
@@ -1,16 +1,31 @@
 package httpd
 
 import (
+	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
+
+// http://feedafever.com/api
+
+type feverResponse struct {
+	XMLName       xml.Name  `json:"-" xml:"response"`
+	Version       int       `json:"api_version" xml:"api_version"`
+	Authorized    int       `json:"auth" xml:"auth"`
+	LastRefreshed feverTime `json:"last_refreshed_on_time,omitempty" xml:"last_refreshed_on_time,omitempty"`
+}
 
 func (z *Service) feverRouter(prefix string) *mux.Router {
 
 	router := mux.NewRouter()
 
-	router.Queries("api", "").Methods(mPost).HandlerFunc(z.feverMux)
+	router.Queries("api", "").Methods(mPost).HandlerFunc(feverMux)
 	router.Queries("api", "").HandlerFunc(notSupported)
 	router.Path(prefix).HandlerFunc(notFound)
 
@@ -18,56 +33,67 @@ func (z *Service) feverRouter(prefix string) *mux.Router {
 
 }
 
-func (z *Service) feverMux(w http.ResponseWriter, req *http.Request) {
+func feverMux(w http.ResponseWriter, req *http.Request) {
 
 	if err := req.ParseForm(); err != nil {
 		http.Error(w, "cannot parse request\n", 400)
 		return
 	}
 
-	// data includes api_key set to md5(email:password)
-	// TODO: return json response with auth: 0
+	useXML := req.URL.Query().Get("api") == "xml"
+
+	rsp := &feverResponse{
+		Version: 3,
+	}
+
 	if apiKey := req.PostFormValue("api_key"); apiKey == "" {
-		http.Error(w, "no api_key given\n", 400)
-		return
-		// } else {
-		// TODO: authenticate
+		rsp.Authorized = 0
+	} else {
+		rsp.Authorized = 1
+		// TODO: authenticate, api_key value is md5(email:password)
 	}
 
-	// "api" key is guarenteed to be in form values
-	nValues := len(req.URL.Query())
-	if nValues == 1 {
-		z.feverAuth(w, req)
-		return
-	} else if nValues > 2 {
-		// TODO: ignore this error
-		http.Error(w, "too many arguments, only one in addition to 'api' is allowed\n", 400)
-		return
+	if rsp.Authorized == 1 {
+
+		for k := range req.URL.Query() {
+
+			switch k {
+			case "api":
+				rsp.LastRefreshed = feverTime{time.Now()} // TODO: get last refreshed
+			case "feeds":
+				// add to response
+			}
+
+		} // loop
+
 	}
 
-	for k := range req.URL.Query() {
-
-		switch k {
-		case "api":
-			continue
-		case "feeds":
-			z.feverFeeds(w, req)
-			return
-		default:
-			http.Error(w, fmt.Sprintf("unrecognized argument: %s\n", k), 400)
-			return
+	if useXML {
+		w.Header().Set(hContentType, "text/xml; charset=utf-8")
+		w.Write([]byte(strings.ToLower(strings.TrimSuffix(xml.Header, "\n")))) // lowercase without trailing newline
+		if err := xml.NewEncoder(w).Encode(&rsp); err != nil {
+			log.Printf("%-7s %-7s cannot serialize fever XML response: %s", logWarn, logName, err.Error())
 		}
-
-	} // loop
+	} else {
+		w.Header().Set(hContentType, "text/json; charset=utf-8")
+		if err := json.NewEncoder(w).Encode(&rsp); err != nil {
+			log.Printf("%-7s %-7s cannot serialize fever JSON response: %s", logWarn, logName, err.Error())
+		}
+	}
 
 }
 
-// http://feedafever.com/api
-
-func (z *Service) feverAuth(w http.ResponseWriter, req *http.Request) {
-	http.Error(w, "auth\n", 200)
+type feverTime struct {
+	time.Time
 }
 
-func (z *Service) feverFeeds(w http.ResponseWriter, req *http.Request) {
-	http.Error(w, "feeds\n", 200)
+func (z feverTime) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("\"%s\"", strconv.FormatInt(z.Unix(), 10))), nil
+}
+
+func (z feverTime) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	e.EncodeToken(start)
+	e.EncodeToken(xml.CharData([]byte(strconv.FormatInt(z.Unix(), 10))))
+	e.EncodeToken(xml.EndElement{Name: start.Name})
+	return nil
 }
