d41ea85cb5a230e9e1c3791ba1297179360bdf40
Karl Ostendorf <karl@ostendorf.com>
2015-12-17 12:19:33 +0100

    replace user with generated output

--- go/src/rakewire/model/user.go
+++ go/src/rakewire/model/user.go
@@ -4,7 +4,6 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"golang.org/x/crypto/bcrypt"
-	"strings"
 )
 
 //go:generate gokv $GOFILE
@@ -16,20 +15,6 @@ type User struct {
 	PasswordHash string
 	FeverHash    string `kv:"FeverHash:1"`
 }
-
-// index names
-const (
-	UserEntity         = "User"
-	UserIndexUsername  = "Username"
-	UserIndexFeverHash = "FeverHash"
-)
-
-const (
-	uID           = "ID"
-	uUsername     = "Username"
-	uPasswordHash = "PasswordHash"
-	uFeverHash    = "FeverHash"
-)
 
 // NewUser creates a new user with the specified username
 func NewUser(username string) *User {
@@ -64,58 +49,4 @@ func (z *User) MatchPassword(password string) bool {
 		return false
 	}
 	return bcrypt.CompareHashAndPassword(hashedPassword, []byte(password)) == nil
-}
-
-// GetName return the name of the entity.
-func (z *User) GetName() string {
-	return UserEntity
-}
-
-// GetID return the primary key of the object.
-func (z *User) GetID() uint64 {
-	return z.ID
-}
-
-// SetID sets the primary key of the object.
-func (z *User) SetID(id uint64) {
-	z.ID = id
-}
-
-// Clear reset all fields to zero/empty
-func (z *User) Clear() {
-	z.ID = 0
-	z.Username = empty
-	z.PasswordHash = empty
-	z.FeverHash = empty
-}
-
-// Serialize serializes an object to a list of key-values.
-func (z *User) Serialize() map[string]string {
-	result := make(map[string]string)
-	setUint(z.ID, uID, result)
-	setString(z.Username, uUsername, result)
-	setString(z.PasswordHash, uPasswordHash, result)
-	setString(z.FeverHash, uFeverHash, result)
-	return result
-}
-
-// Deserialize serializes an object to a list of key-values.
-func (z *User) Deserialize(values map[string]string) error {
-	var errors []error
-	z.ID = getUint(uID, values, errors)
-	z.Username = getString(uUsername, values, errors)
-	z.PasswordHash = getString(uPasswordHash, values, errors)
-	z.FeverHash = getString(uFeverHash, values, errors)
-	if len(errors) > 0 {
-		return errors[0]
-	}
-	return nil
-}
-
-// IndexKeys returns the keys of all indexes for this object.
-func (z *User) IndexKeys() map[string][]string {
-	result := make(map[string][]string)
-	result[UserIndexUsername] = []string{strings.ToLower(z.Username)}
-	result[UserIndexFeverHash] = []string{z.FeverHash}
-	return result
 }
---
+++ go/src/rakewire/model/user_kv.go
@@ -0,0 +1,107 @@
+package model
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// index names
+const (
+	UserEntity         = "User"
+	UserIndexFeverHash = "FeverHash"
+	UserIndexUsername  = "Username"
+)
+
+const (
+	userID           = "ID"
+	userUsername     = "Username"
+	userPasswordHash = "PasswordHash"
+	userFeverHash    = "FeverHash"
+)
+
+// GetName return the name of the entity.
+func (z *User) GetName() string {
+	return UserEntity
+}
+
+// GetID return the primary key of the object.
+func (z *User) GetID() uint64 {
+	return z.ID
+}
+
+// SetID sets the primary key of the object.
+func (z *User) SetID(id uint64) {
+	z.ID = id
+}
+
+// Clear reset all fields to zero/empty
+func (z *User) Clear() {
+	z.ID = 0
+	z.Username = ""
+	z.PasswordHash = ""
+	z.FeverHash = ""
+
+}
+
+// Serialize serializes an object to a list of key-values.
+func (z *User) Serialize() map[string]string {
+	result := make(map[string]string)
+
+	if z.ID != 0 {
+		result[userID] = fmt.Sprintf("%05d", z.ID)
+	}
+
+	if z.Username != "" {
+		result[userUsername] = z.Username
+	}
+
+	if z.PasswordHash != "" {
+		result[userPasswordHash] = z.PasswordHash
+	}
+
+	if z.FeverHash != "" {
+		result[userFeverHash] = z.FeverHash
+	}
+
+	return result
+}
+
+// Deserialize serializes an object to a list of key-values.
+func (z *User) Deserialize(values map[string]string) error {
+	var errors []error
+
+	z.ID = func(fieldName string, values map[string]string, errors []error) uint64 {
+		result, err := strconv.ParseUint(values[fieldName], 10, 64)
+		if err != nil {
+			errors = append(errors, err)
+			return 0
+		}
+		return uint64(result)
+	}(userID, values, errors)
+
+	z.Username = values[userUsername]
+
+	z.PasswordHash = values[userPasswordHash]
+
+	z.FeverHash = values[userFeverHash]
+
+	if len(errors) > 0 {
+		return errors[0]
+	}
+	return nil
+}
+
+// IndexKeys returns the keys of all indexes for this object.
+func (z *User) IndexKeys() map[string][]string {
+	result := make(map[string][]string)
+
+	result[UserIndexFeverHash] = []string{
+		z.FeverHash,
+	}
+
+	result[UserIndexUsername] = []string{
+		z.Username,
+	}
+
+	return result
+}
