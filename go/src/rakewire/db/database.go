b828040579354f8f50987a3d64aa8a764cb20327
Karl Ostendorf <karl@ostendorf.com>
2016-01-05 21:28:29 +0100

    sort func definitions

--- go/src/rakewire/db/database.go
+++ go/src/rakewire/db/database.go
@@ -12,40 +12,60 @@ type Configuration struct {
 
 // Database interface
 type Database interface {
-	UserGetByUsername(username string) (*m.User, error)
-	UserGetByFeverHash(feverhash string) (*m.User, error)
-	UserSave(user *m.User) error
 
-	EntriesGet(feedID uint64) ([]*m.Entry, error)
-	UserFeedSave(userfeed *m.UserFeed) error
-	UserFeedGetAllByUser(userID uint64) ([]*m.UserFeed, error)
-	GroupSave(group *m.Group) error
-	GroupGetAllByUser(userID uint64) ([]*m.Group, error)
+	// Feed
 
-	UserEntryAdd(allEntries []*m.Entry) error
-	UserEntryGetTotalForUser(userID uint64) (uint, error)
-	UserEntryGetByID(userID uint64, ids []uint64) ([]*m.UserEntry, error)
-	UserEntryGetNext(userID uint64, minID uint64, count int) ([]*m.UserEntry, error)
-	UserEntryGetPrev(userID uint64, maxID uint64, count int) ([]*m.UserEntry, error)
-	UserEntryGetUnreadForUser(userID uint64) ([]*m.UserEntry, error)
-	UserEntryGetStarredForUser(userID uint64) ([]*m.UserEntry, error)
-	UserEntrySave(userentries []*m.UserEntry) error
-	UserEntryUpdateReadByFeed(userID, userFeedID uint64, maxTime time.Time, read bool) error
-	UserEntryUpdateStarByFeed(userID, userFeedID uint64, maxTime time.Time, star bool) error
-	UserEntryUpdateReadByGroup(userID, groupID uint64, maxTime time.Time, read bool) error
-	UserEntryUpdateStarByGroup(userID, groupID uint64, maxTime time.Time, star bool) error
-
+	FeedSave(*m.Feed) ([]*m.Entry, error)
 	GetFeedByID(feedID uint64) (*m.Feed, error)
 	GetFeedByURL(url string) (*m.Feed, error)
 	GetFeeds() ([]*m.Feed, error)
 	// GetFetchFeeds get feeds to be fetched within the given max time parameter.
 	GetFetchFeeds(max time.Time) ([]*m.Feed, error)
+
+	// FeedLog
+
 	// GetFeedLog retrieves the past fetch attempts for the feed in reverse chronological order.
 	// If since is equal to 0, return all.
 	GetFeedLog(feedID uint64, since time.Duration) ([]*m.FeedLog, error)
 	FeedLogGetLastFetchTime() (time.Time, error)
+
+	// Entry
+
+	EntriesGet(feedID uint64) ([]*m.Entry, error)
 	GetFeedEntriesFromIDs(feedID uint64, guIDs []string) (map[string]*m.Entry, error)
-	FeedSave(*m.Feed) ([]*m.Entry, error)
+
+	// User
+
+	UserSave(user *m.User) error
+	UserGetByUsername(username string) (*m.User, error)
+	UserGetByFeverHash(feverhash string) (*m.User, error)
+
+	// Group
+
+	GroupSave(group *m.Group) error
+	GroupGetAllByUser(userID uint64) ([]*m.Group, error)
+
+	// UserFeed
+
+	UserFeedSave(userfeed *m.UserFeed) error
+	UserFeedGetAllByUser(userID uint64) ([]*m.UserFeed, error)
+
+	// UserEntry
+
+	UserEntryAdd(allEntries []*m.Entry) error
+	UserEntryGetTotalForUser(userID uint64) (uint, error)
+
+	UserEntryGetByID(userID uint64, ids []uint64) ([]*m.UserEntry, error)
+	UserEntryGetNext(userID uint64, minID uint64, count int) ([]*m.UserEntry, error)
+	UserEntryGetPrev(userID uint64, maxID uint64, count int) ([]*m.UserEntry, error)
+	UserEntryGetUnreadForUser(userID uint64) ([]*m.UserEntry, error)
+	UserEntryGetStarredForUser(userID uint64) ([]*m.UserEntry, error)
+
+	UserEntrySave(userentries []*m.UserEntry) error
+	UserEntryUpdateReadByFeed(userID, userFeedID uint64, maxTime time.Time, read bool) error
+	UserEntryUpdateStarByFeed(userID, userFeedID uint64, maxTime time.Time, star bool) error
+	UserEntryUpdateReadByGroup(userID, groupID uint64, maxTime time.Time, read bool) error
+	UserEntryUpdateStarByGroup(userID, groupID uint64, maxTime time.Time, star bool) error
 }
 
 // DataObject defines the functions necessary for objects to be persisted to the database
