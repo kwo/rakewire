a153dcb1f0f7375e7791a23ded538a3e5a8cc671
Karl Ostendorf <karl@ostendorf.com>
2016-04-16 16:08:42 +0200

    merge object into user

--- model/user.go
+++ model/user.go
@@ -3,8 +3,9 @@ package model
 import (
 	"crypto/md5"
 	"encoding/hex"
-
+	"encoding/json"
 	"golang.org/x/crypto/bcrypt"
+	"strings"
 )
 
 const (
@@ -19,24 +20,26 @@ var (
 	}
 )
 
-// Users is a collection of User objects
-type Users []*User
-
-// ByID maps items to their ID
-func (z Users) ByID() map[string]*User {
-	result := make(map[string]*User)
-	for _, user := range z {
-		result[user.ID] = user
-	}
-	return result
-}
-
 // User defines a system user
 type User struct {
 	ID           string `json:"id"`
 	Username     string `json:"username"`
 	PasswordHash string `json:"passwordhash"`
 	FeverHash    string `json:"feverhash"`
+}
+
+// GetID returns the unique ID for the object
+func (z *User) GetID() string {
+	return z.ID
+}
+
+// MatchPassword checks if the given password matches the user password.
+func (z *User) MatchPassword(password string) bool {
+	hashedPassword, err := hex.DecodeString(z.PasswordHash)
+	if err != nil {
+		return false
+	}
+	return bcrypt.CompareHashAndPassword(hashedPassword, []byte(password)) == nil
 }
 
 // SetPassword updates the password hashes.
@@ -58,11 +61,58 @@ func (z *User) SetPassword(password string) error {
 
 }
 
-// MatchPassword checks if the given password matches the user password.
-func (z *User) MatchPassword(password string) bool {
-	hashedPassword, err := hex.DecodeString(z.PasswordHash)
-	if err != nil {
-		return false
+func (z *User) clear() {
+	z.ID = empty
+	z.Username = empty
+	z.PasswordHash = empty
+	z.FeverHash = empty
+}
+
+func (z *User) decode(data []byte) error {
+	z.clear()
+	if err := json.Unmarshal(data, z); err != nil {
+		return err
 	}
-	return bcrypt.CompareHashAndPassword(hashedPassword, []byte(password)) == nil
+	return nil
+}
+
+func (z *User) encode() ([]byte, error) {
+	return json.Marshal(z)
+}
+
+func (z *User) indexes() map[string][]string {
+	result := make(map[string][]string)
+	result[indexUserUsername] = []string{strings.ToLower(z.Username)}
+	result[indexUserFeverhash] = []string{z.FeverHash}
+	return result
+}
+
+func (z *User) setID(tx Transaction) error {
+	seq := C.getSequences(tx)
+	seq.User = seq.User + 1
+	z.ID = keyEncodeUint(seq.User)
+	return C.putSequences(tx, seq)
+}
+
+// Users is a collection of User objects
+type Users []*User
+
+// ByID maps items to their ID
+func (z Users) ByID() map[string]*User {
+	result := make(map[string]*User)
+	for _, user := range z {
+		result[user.ID] = user
+	}
+	return result
+}
+
+func (z Users) decode(data []byte) error {
+	if err := json.Unmarshal(data, z); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (z Users) encode() ([]byte, error) {
+	return json.Marshal(z)
 }
