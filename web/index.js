045110aaf89dea6beba3f644bec6653d33797754
Karl Ostendorf <karl@ostendorf.com>
2016-05-03 09:25:50 +0200

    add app css

--- web/index.js
+++ web/index.js
@@ -4,6 +4,9 @@ import 'file?name=[name].[ext]!./robots.txt';
 // css
 import './node_modules/normalize.css/normalize.css';
 import './node_modules/milligram/dist/milligram.css';
+import './app.css';
 
 // import global modules
 import 'whatwg-fetch';
+
+import './login';
