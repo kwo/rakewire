47f57e32ce0cf7f69f68aabe2a291a447759a2ef
Karl Ostendorf <karl@ostendorf.com>
2016-05-28 11:50:15 +0200

    update theme colors

--- web/theme.js
+++ web/theme.js
@@ -1,5 +1,5 @@
 import {
-	cyan500, cyan700, pinkA200,
+	blue500, blue700, amber200,
 	grey100, grey300, grey400, grey500,
 	white, darkBlack, fullBlack,
 } from 'material-ui/styles/colors';
@@ -10,10 +10,10 @@ export default {
 	spacing: spacing,
 	fontFamily: 'Roboto, sans-serif',
 	palette: {
-		primary1Color: cyan500,
-		primary2Color: cyan700,
+		primary1Color: blue500,
+		primary2Color: blue700,
 		primary3Color: grey400,
-		accent1Color: pinkA200,
+		accent1Color: amber200,
 		accent2Color: grey100,
 		accent3Color: grey500,
 		textColor: darkBlack,
@@ -21,7 +21,7 @@ export default {
 		canvasColor: white,
 		borderColor: grey300,
 		disabledColor: fade(darkBlack, 0.3),
-		pickerHeaderColor: cyan500,
+		pickerHeaderColor: blue500,
 		clockCircleColor: fade(darkBlack, 0.07),
 		shadowColor: fullBlack,
 	}
