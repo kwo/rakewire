1ad8839d08677dfa367215b4d99e1e3d57a66d29
Karl Ostendorf <karl@ostendorf.com>
2016-07-29 20:12:46 +0200

    disable canonical host check

--- httpd/httpd.go
+++ httpd/httpd.go
@@ -170,7 +170,7 @@ func (z *Service) newHandler() http.Handler {
 		router,
 		CompressHandler(),
 		NoCache(),
-		CanonicalHost(z.publicHostPort, http.StatusMovedPermanently),
+		// CanonicalHost(z.publicHostPort, http.StatusMovedPermanently),
 		AccessLog(),
 		Authenticate(z.database),
 		TimeoutHandler(10*time.Second), // TODO: configurable request timeout
